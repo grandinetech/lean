fbd368a46ee94912928382cb990ec3d04faf3e51
Darius Spr <108625236+Dariusspr@users.noreply.github.com>
2026-01-14 14:34:01 +0200

    format code

--- lean_client/containers/tests/unit_tests/attestation_aggregation.rs
+++ lean_client/containers/tests/unit_tests/attestation_aggregation.rs
@@ -1,10 +1,12 @@
 #[cfg(feature = "devnet2")]
 #[cfg(test)]
 mod tests {
-    use containers::attestation::{AggregatedAttestation, AggregationBits, Attestation, AttestationData};
-    use containers::{Bytes32, Uint64};
+    use containers::attestation::{
+        AggregatedAttestation, AggregationBits, Attestation, AttestationData,
+    };
     use containers::checkpoint::Checkpoint;
     use containers::slot::Slot;
+    use containers::{Bytes32, Uint64};
 
     #[test]
     fn test_aggregated_attestation_structure() {
@@ -21,17 +23,22 @@ mod tests {
             source: Checkpoint {
                 root: Bytes32::default(),
                 slot: Slot(2),
-            }
+            },
         };
 
         let bits = AggregationBits::from_validator_indices(&vec![2, 7]);
         let agg = AggregatedAttestation {
             aggregation_bits: bits.clone(),
-            data: att_data.clone()
+            data: att_data.clone(),
         };
 
         let indices = agg.aggregation_bits.to_validator_indices();
-        assert_eq!(indices.into_iter().collect::<std::collections::HashSet<_>>(), vec![2, 7].into_iter().collect());
+        assert_eq!(
+            indices
+                .into_iter()
+                .collect::<std::collections::HashSet<_>>(),
+            vec![2, 7].into_iter().collect()
+        );
         assert_eq!(agg.data, att_data);
     }
 
@@ -50,7 +57,7 @@ mod tests {
             source: Checkpoint {
                 root: Bytes32::default(),
                 slot: Slot(2),
-            }
+            },
         };
         let att_data2 = AttestationData {
             slot: Slot(6),
@@ -65,7 +72,7 @@ mod tests {
             source: Checkpoint {
                 root: Bytes32::default(),
                 slot: Slot(3),
-            }
+            },
         };
 
         let attestations = vec![
@@ -88,7 +95,12 @@ mod tests {
 
         let agg1 = aggregated.iter().find(|agg| agg.data == att_data1).unwrap();
         let validator_ids1 = agg1.aggregation_bits.to_validator_indices();
-        assert_eq!(validator_ids1.into_iter().collect::<std::collections::HashSet<_>>(), vec![1, 3].into_iter().collect());
+        assert_eq!(
+            validator_ids1
+                .into_iter()
+                .collect::<std::collections::HashSet<_>>(),
+            vec![1, 3].into_iter().collect()
+        );
 
         let agg2 = aggregated.iter().find(|agg| agg.data == att_data2).unwrap();
         let validator_ids2 = agg2.aggregation_bits.to_validator_indices();
@@ -116,7 +128,7 @@ mod tests {
             source: Checkpoint {
                 root: Bytes32::default(),
                 slot: Slot(2),
-            }
+            },
         };
 
         let attestations = vec![Attestation {
