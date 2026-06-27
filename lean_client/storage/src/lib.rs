57d0438bca945c0e1702f8fe9a1ac38e3d0b7a90
Sagar Rana <sagarrana.pvt@gmail.com>
2026-06-28 04:42:50 +0530

    feat: add leanspec consts for db

--- lean_client/storage/src/lib.rs
+++ lean_client/storage/src/lib.rs
@@ -1,14 +1,24 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use database::{Database, DatabaseMode, RestartMessage};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+const BLOCKS_TABLE_NAME: &str = "blocks";
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+const BLOCKS_CREATE_INDEX: &str = "slots_to_block_roots_index";
+
+const STATES_TABLE_NAME: &str = "states";
+
+const STATES_CREATE_INDEX: &str = "slots_to_state_roots_index";
+
+const CHECKPOINTS_TABLE_NAME: &str = "checkpoints";
+
+const CHECKPOINTS_KEY_JUSTIFIED: &str = "justified";
+
+const CHECKPOINTS_KEY_FINALIZED: &str = "finalized";
+
+const CHECKPOINTS_KEY_HEAD: &str = "head";
+
+const CHECKPOINTS_KEY_GENESIS_TIME: &str = "genesis_time";
+
+const SLOT_INDEX_TABLE_NAME: &str = "slot_index";
+
+const STATE_ROOT_INDEX_TABLE_NAME: &str = "state_root_index";
+
